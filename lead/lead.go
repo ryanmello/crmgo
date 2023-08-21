fd59fd0f636cca2197c756b6ddc9845901a5997e
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-21 15:53:28 -0700

    lead functions

--- lead/lead.go
+++ lead/lead.go
@@ -1,1 +1,34 @@
 package lead
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/ryanmello/crmgo/database"
+)
+
+type Lead struct {
+	gorm.Model
+	Name string `json:"name"`
+	Company string `json:"company"`
+	Email string `json:"email`
+	Phone int `json:"phone"`
+}
+
+func GetLeads(c *fiber.Ctx){
+	db := database.GetDB()
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+}
+
+func GetLead(){
+
+}
+
+func NewLead(){
+
+}
+
+func DeleteLead(){
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
 	"github.com/ryanmello/crmgo/database"
+	"github.com/ryanmello/crmgo/lead"
 )
 
 var db *gorm.DB
@@ -15,14 +18,23 @@ func setupRoutes(app *fiber.App){
 	app.Delete(DeleteLead)
 }
 
+type Lead struct {
+
+}
+
 func initDatabase(){
 	database.Connect()
 	db = database.GetDB()
+	db.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated")
 }
 
 func main(){
 	app := fiber.New()
+
+	initDatabase()
 	setupRoutes(app)
 	app.Listen(3000)
 
+	defer db.Close()
 }
