b2c48e51c8a421a6355aeeb3dcf05e16a4f3f88c
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-22 14:58:48 -0700

    lead methods

--- lead/lead.go
+++ lead/lead.go
@@ -8,10 +8,10 @@ import (
 
 type Lead struct {
 	gorm.Model
-	Name string `json:"name"`
+	Name 	string `json:"name"`
 	Company string `json:"company"`
-	Email string `json:"email`
-	Phone int `json:"phone"`
+	Email 	string `json:"email`
+	Phone 	int `json:"phone"`
 }
 
 func GetLeads(c *fiber.Ctx){
@@ -21,14 +21,37 @@ func GetLeads(c *fiber.Ctx){
 	c.JSON(leads)
 }
 
-func GetLead(){
-
+func GetLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.GetDB()
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
 }
 
-func NewLead(){
+func NewLead(c *fiber.Ctx){
+	db := database.GetDB()
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
 
+	db.Create(&lead)
+	c.JSON(lead)
 }
 
-func DeleteLead(){
+func DeleteLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.GetDB()
 
+	var lead Lead
+	db.First(&lead, id)
+	if lead.Name == ""{
+		c.Status(500).Send("No lead found")
+		return
+	}
+
+	db.Delete(&lead)
+	c.Send("Deleted successfully")
 }
