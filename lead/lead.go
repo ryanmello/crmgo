dee55246b7cc0c21314841003456ef25cfac4396
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-22 15:00:49 -0700

    add missing quote

--- lead/lead.go
+++ lead/lead.go
@@ -10,7 +10,7 @@ type Lead struct {
 	gorm.Model
 	Name 	string `json:"name"`
 	Company string `json:"company"`
-	Email 	string `json:"email`
+	Email 	string `json:"email"`
 	Phone 	int `json:"phone"`
 }
 
